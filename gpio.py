9535e3311a27ebdaefeb69d424c07c5a1c02a907
jfernsler <jfernsler@mac.com>
2015-11-02 11:42:46 -0500

    Initial commit - GPIO functionality added

---
+++ gpio.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# gpio.py
+#
+# Jeremy Fernsler
+#   2 - Nov - 2015
+#
+# Simple GPIO control for LEDs
+#   run with sudo
+#
+# takes the following arguments:
+#   READY
+#   CAPTURE
+#   COMPRESS
+#   CAPTURECOMPLETE
+#   COMPRESSCOMPLETE
+#   NOTREADY
+
+import sys, time
+import RPi.GPIO as GPIO
+
+# set the pin numbering method to the board
+#   pincount, not BCM (Broadcom chip)
+GPIO.setmode( GPIO.BOARD )
+
+# supress warnings about channel usage
+GPIO.setwarnings( False )
+
+# Set up LED output pins
+GPIO.setup( 11, GPIO.OUT ) # Ready LED
+GPIO.setup( 13, GPIO.OUT ) # Compressing LED
+GPIO.setup( 15, GPIO.OUT ) # Capture LED
+
+def main () :
+  """ Read the argument, if there's more or less than one
+  just ignore everything and quit """
+
+  argc = len( sys.argv )
+
+  if argc == 2 :
+    changeMode( str( sys.argv[1] ))
+
+def changeMode( mode ) :
+  """ take that argument and parse it for the proper response """
+
+  if mode == 'READY' :
+    cyclefun()
+    GPIO.output( 11, True )
+  elif mode == 'CAPTURE' :
+    GPIO.output( 15, True )
+  elif mode == 'COMPRESS' :
+    GPIO.output( 13, True )
+  elif mode == 'CAPTURECOMPLETE' :
+    GPIO.output( 15, False )
+  elif mode == 'COMPRESSCOMPLETE' :
+    GPIO.output( 13, False )
+  elif mode == 'NOTREADY' :
+    GPIO.output( 11, False )
+
+def cyclefun() :
+  """ this just does a quick cycle animation on the lights
+  nothing special, just a little extra on startup """
+
+  GPIO.output( 11, False )
+  GPIO.output( 13, False )
+  GPIO.output( 15, False )
+
+  time.sleep(.15)
+
+  for i in range(2) :
+    GPIO.output( 15, True )
+    time.sleep(.15)
+    GPIO.output( 15, False )
+    GPIO.output( 13, True )
+    time.sleep(.15)
+    GPIO.output( 13, False )
+    GPIO.output( 11, True )
+    time.sleep(.15)
+    GPIO.output( 11, False)
+
+if __name__ == "__main__" :
+  main()
